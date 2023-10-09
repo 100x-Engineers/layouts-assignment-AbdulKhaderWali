65b86ac5dc652c72abc6093d7bea1440e8655f75
AbdulKhaderWali <abdul.khader5166@gmail.com>
2023-10-09 18:45:01 +0530

    Assignment completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,68 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"neutral-50": "#F9F9F9",
+				"neutral-100": "#f4f4f4",
+				"neutral-200": "#e4e4e4",
+				"neutral-300": "#d3d3d3",
+				"neutral-400": "#a2a2a2",
+				"neutral-500": "#737373",
+				"neutral-600": "#525252",
+				"neutral-700": "#404040",
+				"neutral-800": "#262626",
+				"neutral-900": "#171717",
+				"neutral-950": "#0a0a0a",
+				"neutral-1000": "#000",
+				"blue-default": "#1d9bf0",
+				"blue-hover": "#1871ca",
+				"blue-disabled": "#1e5d87",
+				"blue-wash": "#75beef",
+				"button-stroke": "#546a7a",
+				stroke: "#FFFFFF3D",
+				"searchbar-fill": "#212327",
+				"card-fill": "#16181c",
+				success: "#00be74",
+				"red-error": "#8b141a",
+			},
+
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+			},
+
+			width: {
+				85: "334px",
+				100: "442px",
+				110: "598px",
+			},
+
+			height: {
+				14.5: "58px",
+			},
+
+			boxShadow: {
+				1: "0px 8px 16px 0px rgba(0, 0, 0, 0.25)",
+			},
+
+			lineHeight: {
+				normal: "normal",
+			},
+
+			borderRadius: {
+				"4xl": "30px",
+				"5xl": "65px",
+			},
+
+			borderWidth: {
+				1: "1px",
+			},
+
+			spacing: {
+				23: "93px",
+			},
+		},
+	},
+	plugins: [],
 };
